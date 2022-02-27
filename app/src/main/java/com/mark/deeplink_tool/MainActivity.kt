d49a4541e473f1a8f83989bc12a3bb74f9dd447c
mark <mark@onswitchboard.com>
2022-02-27 12:19:24 -0800

    project init

---
+++ app/src/main/java/com/mark/deeplink_tool/MainActivity.kt
@@ -0,0 +1,43 @@
+package com.mark.deeplink_tool
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.mark.deeplink_tool.ui.theme.DeeplinktoolTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            DeeplinktoolTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Text(text = "Hello $name!")
+}
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    DeeplinktoolTheme {
+        Greeting("Android")
+    }
+}
